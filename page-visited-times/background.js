572531331a5f2ce62b583a5c1c425bf2906e538c
Lechen Tan <tan.lechen@gmail.com>
2020-07-01 21:49:26 -0400

    feat: make the extension universal

--- page-visited-times/background.js
+++ page-visited-times/background.js
@@ -6,53 +6,57 @@
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#FFFFFF'}, function() {});
   
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostContains: 'joinhandshake.com'},
-      }),
-      new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostContains: 'indeed.com'},
-      }),
-      new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostContains: 'linkedin.com'},
-      }),
-      new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostContains: 'dice.com'},
-      }),
-      new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostContains: 'careerbuilder.com'},
-      }),
-      new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostContains: 'monster.com'},
-      }),
-      new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostContains: 'glassdoor.com'},
-      }),
-      new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostContains: 'ziprecruiter.com'},
-      }),
-      new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostContains: 'simplyhired.com'},
-      }),
-      new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostContains: 'upwork.com'},
-      })
-      ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
+  // chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+  //   chrome.declarativeContent.onPageChanged.addRules([{
+  //     conditions: [new chrome.declarativeContent.PageStateMatcher({
+  //       pageUrl: {hostContains: 'joinhandshake.com'},
+  //     }),
+  //     new chrome.declarativeContent.PageStateMatcher({
+  //       pageUrl: {hostContains: 'indeed.com'},
+  //     }),
+  //     new chrome.declarativeContent.PageStateMatcher({
+  //       pageUrl: {hostContains: 'linkedin.com'},
+  //     }),
+  //     new chrome.declarativeContent.PageStateMatcher({
+  //       pageUrl: {hostContains: 'dice.com'},
+  //     }),
+  //     new chrome.declarativeContent.PageStateMatcher({
+  //       pageUrl: {hostContains: 'careerbuilder.com'},
+  //     }),
+  //     new chrome.declarativeContent.PageStateMatcher({
+  //       pageUrl: {hostContains: 'monster.com'},
+  //     }),
+  //     new chrome.declarativeContent.PageStateMatcher({
+  //       pageUrl: {hostContains: 'glassdoor.com'},
+  //     }),
+  //     new chrome.declarativeContent.PageStateMatcher({
+  //       pageUrl: {hostContains: 'ziprecruiter.com'},
+  //     }),
+  //     new chrome.declarativeContent.PageStateMatcher({
+  //       pageUrl: {hostContains: 'simplyhired.com'},
+  //     }),
+  //     new chrome.declarativeContent.PageStateMatcher({
+  //       pageUrl: {hostContains: 'upwork.com'},
+  //     })
+  //     ],
+  //         actions: [new chrome.declarativeContent.ShowPageAction()]
+  //   }]);
+  // });
 });
-const watchlist =['joinhandshake.com','indeed.com','linkedin.com','dice.com','careerbuilder.com','monster.com','glassdoor.com','ziprecruiter.com','simplyhired.com','upwork.com'];
+// const watchlist =['joinhandshake.com','indeed.com','linkedin.com','dice.com','careerbuilder.com','monster.com','glassdoor.com','ziprecruiter.com','simplyhired.com','upwork.com'];
 
 chrome.tabs.onActivated.addListener(function(){
   chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
   let url = tabs[0].url;
-  let host = new URL(url)
-  if (!watchlist.includes(host.hostname.substring(4))){
+  if(url[0]!=='h'){
     chrome.browserAction.setBadgeText({'text': ''  });
     return;
-   }
+  }
+  let host = new URL(url)
+  // if (!watchlist.includes(host.hostname.substring(4))){
+  //   chrome.browserAction.setBadgeText({'text': ''  });
+  //   return;
+  //  }
   url =  url.split('?')[0];
   const current = new Date();
   chrome.storage.sync.get(url, function(result) {
